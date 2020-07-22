da30a05a0a59f95d56e6df24c62033f2f8b9a643
Danny Tuppeny <danny@tuppeny.com>
2020-07-22 09:49:18 +0100

    Switch to a bash script for bots

---
+++ bots/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+BOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$BOTDIR/.."
+
+git clone https://github.com/flutter/flutter --branch dev "$BOTDIR/temp/flutter"
+export PATH="$BOTDIR/temp/flutter:$PATH"
+
+flutter doctor -v
+
+echo "Fetching dependencies..."
+flutter pub get
+
+echo "Analyzing project"
+flutter analyze
+
+echo "Analyzing icon_generator"
+pushd tool/icon_generator
+flutter analyze
+popd
+
+echo "Generating all content"
+# TODO(dantup): Remove "generate" from the end after #8 lands.
+flutter pub run grinder generate
